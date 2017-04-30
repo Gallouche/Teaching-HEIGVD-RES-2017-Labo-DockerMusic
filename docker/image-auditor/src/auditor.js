19a6e733ccd50035fe1038b90d9642c168270216
Gallouche <gallandat.t@gmail.com>
2017-04-30 16:12:53 +0200

    presque fini rest valdation

--- docker/image-auditor/src/auditor.js
+++ docker/image-auditor/src/auditor.js
@@ -1,32 +1,61 @@
 var dgram = require('dgram');
-var client = dgram.createSocket('udp4');
+var clientUDP = dgram.createSocket('udp4');
+var moment = require('moment');
+var tcp = require('net');
+
 var PORT = 6024;
 var MULTICAST_ADDR = '239.255.255.250';
+var TCP_PORT =  2205;
+var TCP_ADDRESS = '0.0.0.0';
 
 var listInstrument = {
-  piano: 'ti-ta-ti',
-  pouet: 'trumpet',
-  trulu: 'flute',
-  gzi-gzi: 'violin',
-  drum: 'boum-boum',
+ 'ti-ta-ti': 'piano' ,
+ 'pouet': 'trumpet',
+  'trulu': 'flute',
+  'gzi-gzi': 'violin',
+  'boum-boum': 'drum'
 }
 
-client.bind(PORT, function() {
-  client.addMembership(MULTICAST_ADDR);
+if (process.argv.length != 2) {
+    console.log('Wrong nomber of arguments, expected 0');
+    return;
+}
+ clientUDP.bind(PORT, function() {
+ clientUDP.addMembership(MULTICAST_ADDR);
   console.log("Listening for broadcasted ads");
 });
 
+var listMusicians = new Map();
 
-client.on('message', function(msg, source) {
-    console.log("Ad has arrived: '" + msg + "'. Source address: " + source.address + ", source port: " + source.port);
-
+ clientUDP.on('message', function(msg, source) {
     msg = JSON.parse(msg);
 
     var musician = {
-      'uuid' : msg.uuid
+      uuid : msg.uuid,
+      instument : listInstrument[msg.sound],
+      arrived : moment().toISOString()
     }
+
+    console.log("Ad has arrived: '" + musician.instument + "'. Source address: " + source.address + ", source port: " + source.port);
+
+    listMusicians.set(musician.uuid,musician)
 });
 
-var listMusicians = new Map();
+var serverTCP = tcp.createServer(function(socket){
+  var musicians = new Array();
+
+  for(var [key, value] of listMusicians){
+    if(moment().diff(value.arrived, 'seconds') > 5){
+      listMusicians.delete(key);
+    }
+    else{
+      musicians.push(value.instument);
+    }
+  }
+
+  socket.end(JSON.stringify(musicians));
+});
+
+serverTCP.listen(TCP_PORT,TCP_ADDRESS);
 
 
--- docker/image-musician/src/musician.js
+++ docker/image-musician/src/musician.js
@@ -7,14 +7,20 @@ const uuid = require('uuid/v1');
 var s = dgram.createSocket('udp4');
 
 var listInstrument = {
-  piano: 'ti-ta-ti',
-  trumpet: 'pouet',
-  flute: 'trulu',
-  violin: 'gzi-gzi',
-  drum: 'boum-boum'
+ 'piano': 'ti-ta-ti',
+  'trumpet': 'pouet',
+  'flute': 'trulu',
+  'violin': 'gzi-gzi',
+  'drum': 'boum-boum'
+}
+
+if (process.argv.length != 3) {
+    console.log('Wrong nomber of arguments, expected 1');
+    return;
 }
 
 var instrArg = process.argv[2];
+
 var uniqueID = uuid();
 
 setInterval(multicastNew, 1000);
@@ -22,10 +28,10 @@ setInterval(multicastNew, 1000);
 function multicastNew() {
   var message = {
     sound : listInstrument[instrArg],
-    uuid: uniqueID
+    uuid : uniqueID
   }
   message = new Buffer(JSON.stringify(message));
-
+  
   s.send(message, 0, message.length, PORT, MULTICAST_ADDR, function (err, bytes) {
     console.log("Sending ad: " + message + " via port " + s.address().port);
   });
