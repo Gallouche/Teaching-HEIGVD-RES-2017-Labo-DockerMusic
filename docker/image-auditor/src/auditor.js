c92e6a5de986a2b3cbefc49065a87c29118610cf
Gallouche <gallandat.t@gmail.com>
2017-04-30 20:01:02 +0200

    finished

--- docker/image-auditor/src/auditor.js
+++ docker/image-auditor/src/auditor.js
@@ -3,11 +3,13 @@ var clientUDP = dgram.createSocket('udp4');
 var moment = require('moment');
 var tcp = require('net');
 
+//definition des variables utiles
 var PORT = 8080;
 var MULTICAST_ADDR = '239.255.22.5';
 var TCP_PORT =  2205;
 var TCP_ADDRESS = '0.0.0.0';
 
+//definition du dictionnaire instruments
 var listInstrument = {
  'ti-ta-ti': 'piano' ,
  'pouet': 'trumpet',
@@ -16,23 +18,28 @@ var listInstrument = {
   'boum-boum': 'drum'
 }
 
+//verifiaction du nombre d'argument fourni
 if (process.argv.length != 2) {
     console.log('Wrong nomber of arguments, expected 0');
     return;
 }
+
+//bind du server sur l'adresse multicast
  clientUDP.bind(PORT, function() {
  clientUDP.addMembership(MULTICAST_ADDR);
   console.log("Listening for broadcasted ads");
 });
 
+//map des musiciens actif
 var listMusicians = new Map();
 
+//a l'arriver d'un message on le parse, on cree un objet musician et on le stock dans la map
  clientUDP.on('message', function(msg, source) {
     msg = JSON.parse(msg);
 
     var musician = {
       'uuid' : msg.uuid,
-      'instument' : listInstrument[msg.sound],
+      'instrument' : listInstrument[msg.sound],
       'activeSince' : moment().toISOString()
     };
 
@@ -41,11 +48,13 @@ var listMusicians = new Map();
     listMusicians.set(musician.uuid,musician)
 });
 
+//on cree le serveur TCP et on fait en sorte qu'il retourne un tableau des musicians
+// actif depuis les 5 derniere secondes.
 var serverTCP = tcp.createServer(function(socket){
   var musicians = new Array();
 
   for(var [key, value] of listMusicians){
-    if(moment().diff(value.arrived, 'seconds') > 5){
+    if(moment().diff(value.activeSince, 'seconds') > 5){
       listMusicians.delete(key);
     }
     else{
@@ -56,6 +65,7 @@ var serverTCP = tcp.createServer(function(socket){
   socket.end(JSON.stringify(musicians));
 });
 
+//on écoute sur le port TCP
 serverTCP.listen(TCP_PORT,TCP_ADDRESS);
 
 
--- docker/image-musician/src/musician.js
+++ docker/image-musician/src/musician.js
@@ -4,6 +4,7 @@ var dgram = require('dgram');
 const uuid = require('uuid/v1');
 var s = dgram.createSocket('udp4');
 
+//dictionnaire des instrument
 var listInstrument = {
  'piano': 'ti-ta-ti',
   'trumpet': 'pouet',
@@ -12,17 +13,22 @@ var listInstrument = {
   'drum': 'boum-boum'
 }
 
+//verification du nombre d'aruments
 if (process.argv.length != 3) {
     console.log('Wrong nomber of arguments, expected 1');
     return;
 }
 
+//récupération de l'argument (instrument souhaiter)
 var instrArg = process.argv[2];
 
+//génération d'un id unique
 var uniqueID = uuid();
 
+//définition d'un intervalle d'envoi des données
 setInterval(multicastNew, 1000);
 
+//fonction de création d'objet, parsing en JSON, et envoie sur l'adresse de multicast
 function multicastNew() {
   var message = {
     sound : listInstrument[instrArg],
